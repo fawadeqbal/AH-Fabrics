d0086799c89349ed6675479798f11eca227c5526
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2024-04-12 11:44:43 +0500

    update meta data

--- src/components/Whatsapp.js
+++ src/components/Whatsapp.js
@@ -4,7 +4,7 @@ import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 function Whatsapp() {
   return (
-    <FloatingWhatsApp phoneNumber="+923149884064" accountName="AH Fabrics"/>
+    <FloatingWhatsApp phoneNumber="+923149884064" accountName="AH Fabrics" notificationDelay={2}/>
   )
 }
 
